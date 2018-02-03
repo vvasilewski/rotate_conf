5dada31678f3aa2757b19a762d94310004a83e25
vvasilewski <vvasilewski@gmail.com>
2018-02-03 11:52:07 +0100

    rotate_conf

---
+++ myconf.js
@@ -0,0 +1,6 @@
+module.exports.myconf = {
+    key: 'AKIAI7ECA6FYQC4JA5FA',
+    secret: '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',
+    domainName: 'wasilewski',
+    domainNameHistory: 'wasilewskihistory'
+};
